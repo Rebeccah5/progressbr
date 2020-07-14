9e09487184a6a2813fa75630b80226d55b63af8c
Rebeccah5 <wambuirebeka@gmail.com>
2020-07-14 19:57:46 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+function update() { 
+
+  var element = document.getElementById("myprogressBar");    
+
+  var width = 1; 
+
+  var identity = setInterval(scene, 50); 
+
+  function scene() { 
+
+    if (width >= 100) { 
+
+      clearInterval(identity); 
+
+    } else { 
+
+      width++;  
+
+      element.style.width = width + '%';  
+
+    } 
+
+  } 
+} s
